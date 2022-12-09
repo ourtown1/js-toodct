91e09eba4db0aec486d3cb8e2cc222282838db7c
ourtown1 <ourtown@hotmail.com>
2022-12-08 21:22:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-toodct
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-toodct)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// define a function that prints a message to the console
+function sayHello() {
+  console.log('Hello, World!');
+}
+
+// call the sayHello function
+sayHello();
